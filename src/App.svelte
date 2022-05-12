c5b2c1a21dfa6e48399a880d48fdc2a47a75849f
robertocarroll <rob@wearethoughtfox.com>
2022-05-12 17:50:37 +0100

    Add content to next section

--- src/App.svelte
+++ src/App.svelte
@@ -29,14 +29,15 @@
 		height: ${innerHeight}px;`;
 	});
 
+
 	// Actions for Scroller components
 	const actions = {
 		first_images: { // Actions for <Scroller/> with id="first-images"
 			image01: () => { // Action for <section/> with data-id="image01"
 				style = "";
-				bgimage = "img/Sky_Frame.jpeg";
+				bgimage = "img/Rail_Technology3.jpg";
 				console.log("first function firing");
-				style = `background: url(${bgimage});
+				style = `background-image: url(${bgimage});
 		height: ${innerHeight}px;`;
 			},
 
@@ -55,7 +56,40 @@
 		height: ${innerHeight}px;`;
 				console.log("third function firing");
 			}
+		},
+		second_images: { // Actions for <Scroller/> with id="first-images"
+			image01: () => { // Action for <section/> with data-id="image01"
+				style = "";
+				bgimage = "img/WS_Impact_Illustrated_Set01.jpg";
+				console.log("first function firing");
+				style = `background-image: url(${bgimage});
+		height: ${innerHeight}px;`;
+			},
+
+			image02: () => { // Action for <section/> with data-id="image02"
+				style = "";
+				bgimage = "img/WS_Impact_Illustrated_Set02.jpg";
+				style = `background-image: url(${bgimage});
+		height: ${innerHeight}px;`;
+				console.log("second function firing");
+
+			},
+
+			image03: () => { // Action for <section/> with data-id="image03"
+				bgimage = "img/WS_Impact_Illustrated_Set03.jpg";
+				style = `background-image: url(${bgimage});
+		height: ${innerHeight}px;`;
+				console.log("third function firing");
+			},
+
+			image03: () => { // Action for <section/> with data-id="image03"
+				bgimage = "img/WS_Impact_Illustrated_Set04.jpg";
+				style = `background-image: url(${bgimage});
+		height: ${innerHeight}px;`;
+				console.log("third function firing");
+			}
 		}
+
 	};
 
 	// Code to run Scroller actions when new caption IDs come into view
@@ -70,6 +104,8 @@
 		});
 	}
 	$: id && runActions(Object.keys(actions)); // Run above code when 'id' object changes
+
+
 
 </script>
 
@@ -147,3 +183,49 @@
 		</figure>
 	</div>
 </section>
+
+
+<Scroller top="{0}" bottom="{1}" bind:id={id['second_images']} bind:count bind:index bind:offset bind:progress
+	{threshold}>
+	<div slot="background" class="bg-full-image" style="{style}">
+		<p>current section: <strong>{index + 1}/{count}</strong></p>
+		<progress value="{count ? (index + 1) / count : 0}"></progress>
+
+		<p>offset in current section</p>
+		<progress value={offset || 0}></progress>
+
+		<p>total progress</p>
+		<progress value={progress || 0}></progress>
+	</div>
+
+	<div slot="foreground">
+		<section data-id="image01">
+			<div class="scroll-box">
+				<p>
+					Section 1 - This chart shows the <strong>area in square kilometres</strong> of each local authority
+					district in
+					the UK. Each circle represents one district. The scale is logarithmic.
+				</p>
+			</div>
+		</section>
+		<section data-id="image02">
+			<div class="scroll-box">
+				<p>
+					Section 2 - This chart shows the <strong>area in square kilometres</strong> of each local authority
+					district in
+					the UK. Each circle represents one district. The scale is logarithmic.
+				</p>
+			</div>
+		</section>
+
+		<section data-id="image03">
+			<div class="scroll-box">
+				<p>
+					Section 3 - This chart shows the <strong>area in square kilometres</strong> of each local authority
+					district in
+					the UK. Each circle represents one district. The scale is logarithmic.
+				</p>
+			</div>
+		</section>
+	</div>
+</Scroller>
